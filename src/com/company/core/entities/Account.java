8a5e86b1be1fe693129dc90c3d66d6908fce6dbb
birelandef <R-Sophiya@yandex.ru>
2015-03-28 18:47:12 +0300

    Tests

---
+++ src/com/company/core/entities/Account.java
@@ -0,0 +1,37 @@
+package com.company.core.entities;
+
+import java.util.GregorianCalendar;
+
+/**
+ * Created by Sophie on 18.03.2015.
+ */
+public class Account extends Person {
+    private String login;
+    private String password;
+
+    public Account(String firstName, String secondName, GregorianCalendar birthDay, String gender, String login, String password) {
+        super(firstName, secondName, birthDay, gender);
+        this.login = login;
+        this.password = password;
+    }
+
+    public Account() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
