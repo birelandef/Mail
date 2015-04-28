ce98fbed4b0ede11287b4aa109c44b0a0cb880de
birelandef <R-Sophiya@yandex.ru>
2015-04-28 13:42:02 +0300

    Add cache

---
+++ test/com/company/core/factory/EntityFactoryImplTest.java
@@ -0,0 +1,36 @@
+package com.company.core.factory;
+
+import static org.junit.Assert.*;
+
+public class EntityFactoryImplTest {
+
+    @org.junit.Test
+    public void testCreatePerson() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCreateAccount() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCreateContact() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCreateFolder() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCreateLetter() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testCreateAttachment() throws Exception {
+
+    }
+}
